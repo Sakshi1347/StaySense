3e197de73c7fbffa06e461a4869a4e3c89d0b188
Sakshi1347 <kumarisakshi12347@gmail.com>
2026-06-20 20:39:08 +0530

    feat:add ui component showcase page

---
+++ app/showcase/page.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { useState } from "react";
+import {
+  Button,
+  Input,
+  Loader,
+  Toast,
+  Modal,
+} from "@/components/ui";
+export default function ShowcasePage() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+  <main className="p-10 space-y-10">
+
+    <h1 className="text-4xl font-bold">
+      UI Component Showcase
+    </h1>
+    <section className="space-y-4">
+
+  <h2 className="text-2xl font-semibold">
+    Button
+  </h2>
+
+  <Button
+    label="Click Me"
+    onClick={() => alert("Button Clicked!")}
+  />
+
+</section>
+<section className="space-y-4">
+
+  <h2 className="text-2xl font-semibold">
+    Input
+  </h2>
+
+  <Input placeholder="Enter your email" />
+
+</section>
+<section className="space-y-4">
+
+  <h2 className="text-2xl font-semibold">
+    Loader
+  </h2>
+
+  <Loader />
+
+</section>
+<section className="space-y-4">
+
+  <h2 className="text-2xl font-semibold">
+    Toast
+  </h2>
+
+  <Toast message="Booking Successful!" />
+
+</section>
+<section className="space-y-4">
+
+  <h2 className="text-2xl font-semibold">
+    Modal
+  </h2>
+
+  <Button
+    label="Open Modal"
+    onClick={() => setIsOpen(true)}
+  />
+
+</section>
+{
+  isOpen && (
+    <Modal title="StaySense AI">
+
+      <p>
+        This is a reusable modal component.
+      </p>
+
+      <div className="mt-4">
+
+        <Button
+          label="Close"
+          onClick={() => setIsOpen(false)}
+        />
+
+      </div>
+
+    </Modal>
+  )
+}
+  </main>
+);
+}
